513c6df75fffe7a525bc62fee6e9c4d893443484
Chris Bailey <me@cbailey.co.uk>
2022-07-11 15:15:03 +0100

    Bump version for distributed OT and prepare for release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Sibyl.MixProject do
   def project do
     [
       app: :sibyl,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
