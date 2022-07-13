0a8b6ecd6f401b327998ec7816184e92392cdffd
Chris Bailey <me@cbailey.co.uk>
2022-07-13 01:04:38 +0100

    Bump version to prep release for cross-function event emission

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Sibyl.MixProject do
   def project do
     [
       app: :sibyl,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
