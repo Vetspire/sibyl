cfaf2a8d65a8455981ff1d8787eb812d6358b457
Chris Bailey <me@cbailey.co.uk>
2023-07-17 23:47:33 +0100

    Update `shell.nix`

--- shell.nix
+++ shell.nix
@@ -2,16 +2,9 @@
 
 with pkgs;
 
-let
-  elixir = (beam.packagesWith erlangR23).elixir.override {
-    version = "1.13.4";
-    sha256 = "1z19hwnv7czmg3p56hdk935gqxig3x7z78yxckh8fs1kdkmslqn4";
-  };
-in
-
 mkShell {
   buildInputs = [
-    elixir
+    pkgs.elixir_1_12
     pkgs.inotify-tools
     pkgs.docker-compose
   ];
