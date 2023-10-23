9371a8d867f1bc1f5fce96b70cc3395c05e65b68
Chris Bailey <me@cbailey.co.uk>
2023-10-23 17:02:32 +0100

    Update `shell.nix` and `.tool-versions`

--- shell.nix
+++ shell.nix
@@ -2,9 +2,20 @@
 
 with pkgs;
 
+let
+  inherit (lib) optional optionals;
+
+  # `sha256` can be programmatically obtained via running the following:
+  # `nix-prefetch-url --unpack https://github.com/elixir-lang/elixir/archive/v${version}.tar.gz`
+  elixir_1_15_7 = (beam.packagesWith pkgs.erlangR26).elixir_1_15.override {
+    version = "1.15.7";
+    sha256 = "0yfp16fm8v0796f1rf1m2r0m2nmgj3qr7478483yp1x5rk4xjrz8";
+  };
+in
+
 mkShell {
   buildInputs = [
-    pkgs.elixir_1_12
+    elixir_1_15_7
     pkgs.inotify-tools
     pkgs.docker-compose
   ];
